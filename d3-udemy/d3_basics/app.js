0229e1d4afceb06f00ca23f9f476102369ca2ddc
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-02 16:50:12 -0500

    practice loading data

--- d3-udemy/d3_basics/app.js
+++ d3-udemy/d3_basics/app.js
@@ -1,31 +1,28 @@
-var dataset = [10, 20, 30, 40, 50];
+d3.csv("./data.csv").then(function(data){
+  console.log(data);
+  generate(data.columns);
+})
 
-var el = d3.select('body')
-    .selectAll('p')
-      .data(dataset)
-      .enter()
-      .append('p')
-      .text(function(d) {
-        return 'This paragraph is bound to the number ' + d;})
-      .style('color', function(d){
-        if (d > 25){
-          return 'red';
-        } else {
-          return 'blue';
-        }})
-      .classed('foo', function(d){
-        if (d > 25){
-          return false;
-        } else {
-          return true;
-        }
-      });
+function generate(dataset){}
+  var el = d3.select('body')
+      .selectAll('p')
+        .data(dataset)
+        .enter()
+        .append('p')
+        .text(function(d) {
+          return 'This paragraph is bound to the number ' + d;})
+        .style('color', function(d){
+          if (d > 25){
+            return 'red';
+          } else {
+            return 'blue';
+          }})
+        .classed('foo', function(d){
+          if (d > 25){
+            return false;
+          } else {
+            return true;
+          }
+        });
 
-  // .append('p')
-  //   .attr('class', 'foo')
-  //   .classed('foo', true)
-  //   .classed('bar', true)
-  //   .text('Hello World!')
-  //   .style('color', 'blue');
-
-console.log(el);
+    console.log(el);
