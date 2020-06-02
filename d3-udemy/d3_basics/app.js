5c49e6c0a2a184e1cff20a11e639ac079f1b621a
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-02 16:23:51 -0500

    playing with selections.

--- d3-udemy/d3_basics/app.js
+++ d3-udemy/d3_basics/app.js
@@ -1,2 +1,17 @@
-var el = d3.select( 'body' ).append( 'p' );
+var dataset = [10, 20, 30, 40, 50];
+
+var el = d3.select('body')
+    .selectAll('p')
+      .data(dataset)
+      .enter()
+      .append('p')
+      .text('Hahahahaha world')
+
+  // .append('p')
+  //   .attr('class', 'foo')
+  //   .classed('foo', true)
+  //   .classed('bar', true)
+  //   .text('Hello World!')
+  //   .style('color', 'blue');
+
 console.log(el);
