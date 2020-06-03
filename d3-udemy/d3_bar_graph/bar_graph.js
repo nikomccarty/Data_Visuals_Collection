9218c697202818e8b5c976217dd51fc60fc6abde
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-03 17:50:16 -0500

    started work on bar graph in D3.js

---
+++ d3-udemy/d3_bar_graph/bar_graph.js
@@ -0,0 +1,33 @@
+d3.csv("./data.csv").then(function(data){
+  console.log(data);
+  // generate(data.columns);
+});
+
+d3.json('./data.json').then(function(data){
+  //console.log(data);
+  //generate(data);
+});
+
+function generate(dataset){}
+  var el = d3.select('body')
+      .selectAll('p')
+        .data(dataset)
+        .enter()
+        .append('p')
+        .text(function(d) {
+          return 'This paragraph is bound to the number ' + d;})
+        .style('color', function(d){
+          if (d > 25){
+            return 'red';
+          } else {
+            return 'blue';
+          }})
+        .classed('foo', function(d){
+          if (d > 25){
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+    console.log(el);
