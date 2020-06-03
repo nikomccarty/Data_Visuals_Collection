b2ec8b6a68fde558af600ee600768a76c9b98c89
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-02 23:26:51 -0500

    playing with data in csv and json

--- d3-udemy/d3_basics/app.js
+++ d3-udemy/d3_basics/app.js
@@ -1,7 +1,12 @@
 d3.csv("./data.csv").then(function(data){
   console.log(data);
-  generate(data.columns);
-})
+  // generate(data.columns);
+});
+
+d3.json('./data.json').then(function(data){
+  //console.log(data);
+  //generate(data);
+});
 
 function generate(dataset){}
   var el = d3.select('body')
