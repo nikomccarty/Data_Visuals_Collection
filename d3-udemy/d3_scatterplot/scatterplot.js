0018a0e22c06fc66ad45affe91911e13ed846147
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-04 17:02:58 -0500

    playing with scatterplot scales.

--- d3-udemy/d3_scatterplot/scatterplot.js
+++ d3-udemy/d3_scatterplot/scatterplot.js
@@ -1,5 +1,5 @@
 var data = [
-  [10, 200],
+  [50, 200],
   [210, 140],
   [222, 300],
   [70, 160],
@@ -8,8 +8,9 @@ var data = [
   [110, 310]
 ];
 
-var chart_width = 800;
-var chart_height = 400;
+var chart_width = 600;
+var chart_height = 300;
+var padding = 20;
 
 //First, I will try to make a scatterplot on my own (no help)
 
@@ -19,18 +20,40 @@ var svg = d3.select('#chart')
   .attr('width', chart_width)
   .attr('height', chart_height);
 
+// Create Scales
+var x_scale = d3.scaleLinear()
+                .domain([0, d3.max(data, function(d){
+                  return d[0];
+                })])
+                .range([ padding, chart_width - padding * 2 ]);
+
+var y_scale = d3.scaleLinear()
+                .domain([0, d3.max(data, function(d){
+                  return d[1];
+                })])
+                .range([padding, chart_height-padding]);
+
+var r_scale = d3.scaleLinear()
+                .domain([0, d3.max(data, function(d){
+                  return d[1];
+                })])
+                .range([5, 15]);
+
 //Create circles that are bound to my data.
 svg.selectAll('circle')
   .data(data)
   .enter()
   .append('circle')
   .attr('cx', function(d){
-    return d[0];
+    return x_scale(d[0]);
   })
   .attr('cy', function(d){
-    return chart_height - d[1];
+    return chart_height - y_scale(d[1]);
   })
-  .attr('r', 10);
+  .attr('r', function(d){
+    return r_scale(d[1]);
+  })
+  .attr('fill', '#A8E637');
 
   // Now I am watching the video...I will add any alterations here.
   // No changes to note. Except, I need to add labels.
@@ -45,8 +68,8 @@ svg.selectAll('text')
     return d.join(',');
   })
   .attr('x', function(d){
-    return d[0] + 13;
+    return x_scale(d[0]) - 20;
   })
   .attr('y', function(d){
-    return chart_height - d[1];
+    return chart_height - y_scale(d[1]);
   });
