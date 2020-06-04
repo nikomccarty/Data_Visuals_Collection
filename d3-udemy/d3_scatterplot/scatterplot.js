d37fa1a84857010e4392b00411675ea207601fc8
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-04 14:54:33 -0500

    made a scatterplot, added labels.

--- d3-udemy/d3_scatterplot/scatterplot.js
+++ d3-udemy/d3_scatterplot/scatterplot.js
@@ -13,11 +13,13 @@ var chart_height = 400;
 
 //First, I will try to make a scatterplot on my own (no help)
 
+//Create SVG element
 var svg = d3.select('#chart')
   .append('svg')
   .attr('width', chart_width)
   .attr('height', chart_height);
 
+//Create circles that are bound to my data.
 svg.selectAll('circle')
   .data(data)
   .enter()
@@ -29,3 +31,22 @@ svg.selectAll('circle')
     return chart_height - d[1];
   })
   .attr('r', 10);
+
+  // Now I am watching the video...I will add any alterations here.
+  // No changes to note. Except, I need to add labels.
+
+  // Create labels
+svg.selectAll('text')
+  .data(data)
+  .enter()
+  .append('text')
+  .attr('font-size', 14)
+  .text(function(d){
+    return d.join( ',');
+  })
+  .attr('x', function(d){
+    return d[0] + 13;
+  })
+  .attr('y', function(d){
+    return chart_height - d[1];
+  });
