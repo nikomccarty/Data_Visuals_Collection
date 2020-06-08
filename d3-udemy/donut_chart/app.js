165fa9d65bb13302ac1732640123631d845d9f1b
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-08 16:42:50 -0500

    pie chart (gross) in D3.js

---
+++ d3-udemy/donut_chart/app.js
@@ -0,0 +1,46 @@
+// Data
+var data            =   [ 25, 25, 25, 25 ];
+var chart_width     =   600;
+var chart_height    =   600;
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Pie variable using d3.pie
+var pie = d3.pie();
+
+// Arc variable
+var outer_radius = chart_width / 2;
+var inner_radius = 0;
+var arc = d3.arc()
+            .innerRadius(inner_radius)
+            .outerRadius(outer_radius);
+
+var svg = d3.select('#chart')
+            .append('svg')
+            .attr('height', chart_height)
+            .attr('width', chart_width);
+
+// Groups
+var arcs = svg.selectAll('g.arc')
+              .data(pie(data))
+              .enter()
+              .append('g')
+              .classed('arc', true)
+              .attr('transform',
+                "translate(" + outer_radius + "," + chart_height / 2 + ")"
+              );
+
+arcs.append('path')
+    .attr('fill', function(d, i){
+      return color(i);
+    })
+    .attr('d', arc);
+
+// Labels
+arcs.append('text')
+    .attr('transform', function(d,i){
+      return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr('text-anchor', 'text-middle')
+    .text(function(d){
+      return d.value;
+    });
