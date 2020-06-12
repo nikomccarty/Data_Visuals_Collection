1b3c06eafae7cbbb1dff9edb448918baf27dbe08
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-11 23:26:19 -0500

    work on updatable bar chart from scratch

---
+++ 20200611_updating_bar_graph/app.js
@@ -0,0 +1,54 @@
+// Goal: Make a bar chart from scratch. Then, I will figure out how to add transitions and animations to update / sort the data.
+
+// Also add a nice title and legend and buttons for the sorting.
+var data = [
+  {Q1: 10},
+  {Q2: 30},
+  {Q3: 60},
+  {Q4: 95}
+];
+
+var w =  800;
+var h = 400;
+var padding = 10;
+
+// Set up scales
+var y_scale = d3.scaleLinear()
+                .domain([0, d3.max(data)])
+                .rangeRound([0, h]);
+
+var x_scale = d3.scaleBand()
+                .domain(d3.range(data.length))
+                .rangeRound([0, w])
+                .paddingInner(0.05);
+
+// Set up x AND y axes
+var x_axis = d3.axisBottom()
+               .scale(x_scale);
+
+// Add svg element to #chart hold bar Chart
+var svg = d3.select('#chart')
+            .append('svg')
+            .attr('width', w)
+            .attr('height', h);
+
+// Add rectangles to the page
+svg.selectAll('rect')
+   .data(data)
+   .enter()
+   .append('rect')
+   .attr('width', x_scale.bandwidth())
+   .attr('height', function(d){
+     return y_scale(d);
+   })
+   .attr('x', function(d, i){
+     return x_scale(i);
+   })
+   .attr('y', function(d){
+     return h - y_scale(d);
+   });
+
+svg.append('g')
+   .attr('class', 'x-axis')
+   .call(x_axis)
+   .attr('transform', 'translate(0, 100)');
