2eac9e9b83b3321ee7c72084fdb5bd3a819b441d
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-06-17 16:39:02 -0500

    more simple bar graph

--- 20200611_simple_bar_graph/app.js
+++ 20200611_simple_bar_graph/app.js
@@ -1,55 +1,52 @@
 // var data = Array.from({length: 10}, () => Math.floor(Math.random() * 50));
-var rowConverter = function() {
-  return {
-    name: d.name,
-    value: +d.value
-  };
-};
+// d3.csv('alphabet.csv', d3.autoType).then(function(data) {
+//   data.forEach(function(d) {
+//     d.letter = d.letter;
+//     d.frequency = +d.frequency;
+//   });
+//   console.log(data);
+// });
 
-var data = d3.csvParse('data.csv').text(),
+//var data = d3.csv('alphabet.csv', d3.autoType);
 
-.then(function(d) {
-  return {
-    name: d.name,
-    value: parseFloat(d.value)
-  }
-});
+var data = [15, 20, 30, 40, 50]
 
 var w =  800;
 var h = 400;
 var margin = ({top: 20, right: 30, bottom: 30, left: 40});
+var padding = 20;
 
-var yScale = d3.scaleLinear()
+var y = d3.scaleLinear()
      .domain([0, d3.max(data)])
-     .range([0, h-margin.top]);
+     .range([0, h - padding]);
 
-var xScale = d3.scaleBand()
+var x = d3.scaleBand()
      .domain(d3.range(data.length))
-     .range([0, w])
+     .range([padding, w - padding])
      .round(true)
      .padding(0.1);
 
-var yAxis = d3.axisLeft()
-
 var svg = d3.select('#chart')
      .append('svg')
-     .attr('width', w)
-     .attr('height', h);
+     .attr('width', w - padding)
+     .attr('height', h - padding);
+
+var yAxis = d3.scaleLinear()
+     .domain([0, d3.max(data)])
+     .range([h - padding, padding]);
+
+svg.append('g')
+  .attr('transform', 'translate(' + (padding + 10)  + ', 0)')
+  .call(d3.axisLeft(yAxis))
 
 svg.selectAll('rect')
   .data(data)
   .enter()
   .append('rect')
-  .attr('x', function(d,i){
-    return xScale(i);
-  })
-  .attr('y', function(d){
-    return h - yScale(d);
-  })
-  .attr('width', xScale.bandwidth())
-  .attr('height', function(d){
-    return yScale(d);
-  })
+  .attr('x', (d,i) => x(i) + padding)
+  .attr('y', d => h - y(d) - padding)
+  .attr('width', x.bandwidth())
+  .attr('height', d => y(d))
   .attr('fill', function(d){
     return 'rgb(0, 0, ' + Math.round(d*10) + ')';
   });
@@ -58,13 +55,7 @@ svg.selectAll('text')
   .data(data)
   .enter()
   .append('text')
-  .text(function(d){
-    return d;
-  })
-  .attr('x', function(d,i){
-    return xScale(i) + 32;
-  })
-  .attr('y', function(d){
-    return h - yScale(d) + 20;
-  })
+  .text(d => d)
+  .attr('x', (d,i) => x(i) + x.bandwidth() / 2)
+  .attr('y', d => h - y(d) + 15)
   .attr('text-anchor', 'middle');
