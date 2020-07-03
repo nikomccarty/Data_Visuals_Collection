c5a001fa4e5cff6d09ee459dbdd46fa685de2e9e
nmccarty3 <NMCCARTY@CALTECH.EDU>
2020-07-02 19:41:17 -0500

    updates to iris scatterplot

--- 20200702_scatterplot_interactive/app.js
+++ 20200702_scatterplot_interactive/app.js
@@ -1,84 +1,83 @@
-// Letter frequency bar chart in d3, with ordinal x-axis
-// From Mike Bostock's Observable tutorial, but written in normal JS
-// https://observablehq.com/@d3/lets-make-a-bar-chart/4?collection=@d3/lets-make-a-bar-chart
+// OBJECTIVES
+// Make a simple scatterplot using the iris.csv dataset. Start with just the petal length and width.
+// Color each point based on the species
+// Add an interactive option for the user to plot, instead, sepal length and width for each species.
+// Add buttons or a dropdown to display only one species, or two species, or all three, at a time.
 
-d3.csv('alphabet.csv', d3.autoType)
+var w =  800;
+var h = 400;
+var margin = ({top: 20, right: 30, bottom: 30, left: 40});
+var padding = 20;
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+d3.csv('iris.csv', d3.autoType)
   .then(function(data) {
-    var w =  800;
-    var h = 400;
-    var margin = ({top: 20, right: 30, bottom: 30, left: 40});
-    var padding = 20;
 
     var y = d3.scaleLinear()
-         .domain([0, d3.max(data, d => d.frequency)])
+         .domain(d3.extent(data, d => d.petal_length))
          .range([h - margin.bottom, margin.top]);
 
-    var x = d3.scaleBand()
-         .domain(data.map(d => d.letter))
-         .rangeRound([margin.left, w - margin.right])
-         .padding(0.1);
+    var x = d3.scaleLinear()
+         .domain(d3.extent(data, d => d.petal_width))
+         .range([margin.left, w - margin.right]);
 
     var svg = d3.select('#chart')
          .append('svg')
          .attr('width', w )
          .attr('height', h );
 
-    // svg.append("g")
-    //      .attr("transform", 'translate(0,' + (h - margin.bottom) + ')')
-    //      .call(d3.axisBottom(x));
+    const g = d3.select('svg').append('g');
 
-    // svg.append("g")
-    //      .attr("transform", 'translate(' + margin.left + ',0)' )
-    //      .call(d3.axisLeft(y));
+    var xAxis = d3.axisBottom()
+         .scale(x)
+         .ticks(5);
 
-    yTitle = g => g.append('text')
-         .attr('font-family', 'sans-serif')
-         .attr('font-size', 10)
-         .attr('y', 10)
-         .text('↑ Frequency');
-
-    yAxis = g => g
-         .attr('transform', 'translate(' + margin.left + ')')
-         .call(d3.axisLeft(y).ticks(null, "%"))
-         .call(g => g.select(".domain").remove());
-
-    xAxis = g => g
-         .attr('transform', 'translate(0,' + (h - margin.bottom) + ')')
-         .call(d3.axisBottom(x).tickSizeOuter(0));
-
-    var bar = svg.selectAll('rect')
-         .data(data)
-         .enter()
-         .append('rect')
-         .attr('x', d => x(d.letter))
-         .attr('y', d => y(d.frequency))
-         .attr('width', x.bandwidth())
-         .attr('height', d => y(0) - y(d.frequency))
-         .attr('fill', 'steelblue');
+    var yAxis = d3.axisLeft()
+         .scale(y)
+         .ticks(5);
 
     svg.append('g')
-         .call(xAxis);
-
-    svg.append('g')
+         .attr('class', 'axis')
+         .attr('transform', 'translate(' + margin.left + ',0)')
          .call(yAxis);
 
+    // yTitle = g => g.append('text')
+    //      .attr('font-family', 'sans-serif')
+    //      .attr('font-size', 10)
+    //      .attr('y', 10)
+    //      .text('↑ Petal Length');
+    //
+    // xTitle = g => g.append('text')
+    //      .attr('font-family', 'sans-serif')
+    //      .attr('font-size', 10)
+    //      .attr('x', w - margin.right)
+    //      .text('↑ Petal Width');
+    //
+
+    //
+    //
+
+    const circle = svg.selectAll('circle')
+         .data(data)
+         .enter()
+         .append('circle')
+         .attr('r', 5)
+         .attr('cx', d => x(d.petal_width))
+         .attr('cy', d => y(d.petal_length))
+         .attr('fill', d => color(d.species));
+
+    // svg.append('g')
+    //      .call(xAxis);
+    //
+    // svg.append('g')
+    //      .call(yAxis);
+    //
     svg.append('g')
          .call(yTitle);
-
-    d3.select('p')
-      .on('click', function() {
-        groups.selectAll('rect')
-              .sort((a, b) => d3.ascending(a.frequency, b.frequency))
-              .attr('x', d => x(d.letter))
-              .attr('y', d => y(d.frequency))
-              .attr('width', x.bandwidth())
-              .attr('height', d => y(0) - y(d.frequency))
-            });
-
   })
   .catch(function(error){
      console.log('There is an error with the data.')
-  })
+  });
 
 
 
